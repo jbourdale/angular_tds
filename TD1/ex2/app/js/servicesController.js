f042930d87a3ce2dbad23b4949ceb8842141b000
jbourdale <jues.bourdale@gmail.com>
2017-02-03 10:55:48 +0100

    Angular App Finished

--- TD1/ex2/app/js/servicesController.js
+++ TD1/ex2/app/js/servicesController.js
@@ -1,5 +1,20 @@
-angular.module("serviceApp").controller("serviceCtrl", [function(){
+angular.module("serviceApp").controller("serviceCtrl", ['$http', function($http){
 
+    var self = this;
+
+    this.reduc = 0;
+    console.log("Loading JSON file");
+    this.promo;
+
+    $http.get('assets/promo.json').then(function(response){
+        console.log("JSON : "+JSON.stringify(response.data));
+        self.promo = response.data;
+    }, function(){});
+
+    this.promoEnabled = false;
+    this.promoCode="";
+
+    this.nbActifs = 1;
 
     this.services =
     [
@@ -24,12 +39,30 @@ angular.module("serviceApp").controller("serviceCtrl", [function(){
 
 
     this.total = function(){
-
-
+        var tot=0;
+        for(var i = 0; i<self.services.length; i++){
+            if(self.services[i].active){
+               tot+= self.services[i].price;
+            }
+        }
+        return tot - (tot * self.reduc);
     };
 
-    this.toggleActive = function(){
-
-
+    this.toggleActive = function(service){
+        service.active = !service.active;
+        if(service.active) self.nbActifs++;
+        else self.nbActifs--;
     };
+
+    this.checkPromo = function(){
+        if(self.promoEnabled) {
+            if(self.promo[self.promoCode]){
+                self.reduc = self.promo[self.promoCode];
+            }else{
+                self.reduc = 0;
+            }
+        }else{
+            self.reduc = 0;
+        }
+    }
 }]);
